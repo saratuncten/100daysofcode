1ca5f80e0740746bb9a74fae1619590fce21e31f
Sara Carpenter <44247793+saratuncten@users.noreply.github.com>
2024-01-31 09:53:59 -0600

    Update day1_band_name_generator.py

--- daily_projects/day1_band_name_generator.py
+++ daily_projects/day1_band_name_generator.py
@@ -1,1 +1,4 @@
-
+print("Welcome to the Band Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your favorite pet?\n")
+print(f"Your band name could be: The {city} {pet}s")
